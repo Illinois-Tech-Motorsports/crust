4384235fb27a86104aaac3ed54d1272aab329759
Noah Husby <noahhusby@gmail.com>
2023-06-13 15:44:40 -0500

    refact: updated post-image.sh

--- scripts/post-image.sh
+++ scripts/post-image.sh
@@ -2,15 +2,8 @@
 
 set -e
 
-BOARD_DIR="$(dirname $0)"
-BOARD_NAME="$(basename ${BOARD_DIR})"
-GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
+GENIMAGE_CFG="../resources/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
-
-# Pass an empty rootpath. genimage makes a full copy of the given rootpath to
-# ${GENIMAGE_TMP}/root so passing TARGET_DIR would be a waste of time and disk
-# space. We don't rely on genimage to build the rootfs image, just to insert a
-# pre-built one in the disk image.
 
 trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
 ROOTPATH_TMP="$(mktemp -d)"
