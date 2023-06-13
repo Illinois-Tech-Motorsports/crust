f609c990ca3c62b78943b10c7d1eea815009dbe7
Noah Husby <noahhusby@gmail.com>
2023-06-13 16:00:19 -0500

    refact: added crust inject

--- scripts/post-image.sh
+++ scripts/post-image.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+CRUST_INJECT_DIR="${BINARIES_DIR}/crust-inject"
 GENIMAGE_CFG="../resources/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+mkdir -p "${CRUST_INJECT_DIR}"
+cp "../resources/cmdline.txt" "${CRUST_INJECT_DIR}/cmdline.txt"
+cp "../resources/config.txt" "${CRUST_INJECT_DIR}/config.txt"
 
 trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
 ROOTPATH_TMP="$(mktemp -d)"
